120c6a2227e88d827bd49b0461d87e015d44c022
Emanuel Vargas <evargas@foundationsoft.com>
2022-01-12 16:42:36 -0500

    add calls to get branches

--- src/FoundationSprintly/SprintlyBranchSearch.tsx
+++ src/FoundationSprintly/SprintlyBranchSearch.tsx
@@ -1,21 +1,32 @@
 import * as React from 'react';
 
 import { getClient, IExtensionDataManager } from 'azure-devops-extension-api';
-import { GitRef } from 'azure-devops-extension-api/Git';
+import { GitRef, GitRestClient } from 'azure-devops-extension-api/Git';
 
 import {
     ObservableArray,
     ObservableValue,
 } from 'azure-devops-ui/Core/Observable';
+import { TeamProjectReference } from 'azure-devops-extension-api/Core';
 
 import { Button } from 'azure-devops-ui/Button';
 import { ButtonGroup } from 'azure-devops-ui/ButtonGroup';
 import { Card } from 'azure-devops-ui/Card';
 import { Page } from 'azure-devops-ui/Page';
 import { TextField, TextFieldWidth } from 'azure-devops-ui/TextField';
-import { ColumnSorting, ITableColumn, sortItems, SortOrder, Table } from 'azure-devops-ui/Table';
+import {
+    ColumnSorting,
+    ITableColumn,
+    sortItems,
+    SortOrder,
+    Table,
+} from 'azure-devops-ui/Table';
+
+import * as Common from './SprintlyCommon';
 
 //#region "Observables"
+const totalRepositoriesToProcessObservable: ObservableValue<number> =
+    new ObservableValue<number>(0);
 const searchObservable = new ObservableValue<string>('');
 const nameColumnWidthObservable: ObservableValue<number> =
     new ObservableValue<number>(-30);
@@ -23,7 +34,14 @@ const repositoryColumnWidthObservable: ObservableValue<number> =
     new ObservableValue<number>(-40);
 //#endregion "Observables"
 
+const userSettingsDataManagerKey: string = 'user-settings';
+const systemSettingsDataManagerKey: string = 'system-settings';
+
+let repositoriesToProcess: string[] = [];
+
 export interface ISprintlyBranchSearchPageState {
+    userSettings?: Common.IUserSettings;
+    systemSettings?: Common.ISystemSettings;
     searchResultBranches: ObservableArray<GitRef>;
 }
 
@@ -103,6 +121,36 @@ export default class SprintlyBranchSearchPage extends React.Component<
         this.dataManager = props.dataManager;
     }
 
+    public async componentDidMount(): Promise<void> {
+        await this.initializeComponent();
+    }
+
+    private async initializeComponent(): Promise<void> {
+        const userSettings: Common.IUserSettings | undefined =
+            await Common.getUserSettings(
+                this.dataManager,
+                userSettingsDataManagerKey
+            );
+        const systemSettings: Common.ISystemSettings | undefined =
+            await Common.getSystemSettings(
+                this.dataManager,
+                systemSettingsDataManagerKey
+            );
+
+        this.setState({
+            userSettings,
+            systemSettings,
+        });
+
+        repositoriesToProcess = Common.getSavedRepositoriesToView(
+            this.state.userSettings,
+            this.state.systemSettings
+        );
+
+        totalRepositoriesToProcessObservable.value =
+            repositoriesToProcess.length;
+    }
+
     private renderNameCell(
         rowIndex: number,
         columnIndex: number,
@@ -119,6 +167,30 @@ export default class SprintlyBranchSearchPage extends React.Component<
         tableItem: GitRef
     ): JSX.Element {
         return <></>;
+    }
+
+    private async searchAction(): Promise<void> {
+        const searchTerm: string = searchObservable.value.trim();
+        if (searchTerm && totalRepositoriesToProcessObservable.value > 0) {
+            for (const repositoryId of repositoriesToProcess) {
+                const repositoryBranches: GitRef[] = await getClient(
+                    GitRestClient
+                ).getRefs(
+                    repositoryId,
+                    undefined,
+                    Common.repositoryHeadsFilter,
+                    undefined,
+                    undefined,
+                    undefined,
+                    undefined,
+                    undefined,
+                    'feature/'
+                );
+                for (const branch of repositoryBranches) {
+                    console.log(branch);
+                }
+            }
+        }
     }
 
     private onSize(event: MouseEvent, index: number, width: number): void {
@@ -141,16 +213,14 @@ export default class SprintlyBranchSearchPage extends React.Component<
                     <Button
                         text='Search'
                         primary={true}
-                        onClick={() => alert(searchObservable.value)}
+                        onClick={async () => await this.searchAction()}
                     />
                 </ButtonGroup>
                 <Page>
                     <Card className='bolt-table-card bolt-card-white'>
                         <Table
                             columns={this.columns}
-                            behaviors={[
-                                this.sortingBehavior,
-                            ]}
+                            behaviors={[this.sortingBehavior]}
                             itemProvider={this.state.searchResultBranches}
                         />
                     </Card>
